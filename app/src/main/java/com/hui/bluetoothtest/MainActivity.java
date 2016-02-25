bf7867cb2528ffe0cbced69ce983ec4415c361eb
wangjianhui <wangjianhui@live.com>
2016-02-25 20:52:39 +0800

    add the button effect

--- app/src/main/java/com/hui/bluetoothtest/MainActivity.java
+++ app/src/main/java/com/hui/bluetoothtest/MainActivity.java
@@ -40,14 +40,14 @@ import java.util.Set;
 import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener, AdapterView.OnItemClickListener {
-        public static final int REQUEST_NDVI = 0;
-        public static final int REQUEST_LAI = 1;
-        public static final int REQUEST_BIO = 2;
-        public static final int REQUEST_CHL = 3;
-        public static final int REQUEST_N = 4;
-        public static final int REQUEST_HEIGHT = 5;
-        public static final int REQUEST_TOBJ = 6;
-        public static final int REQUEST_TENV = 7;
+    public static final int REQUEST_NDVI = 0;
+    public static final int REQUEST_LAI = 1;
+    public static final int REQUEST_BIO = 2;
+    public static final int REQUEST_CHL = 3;
+    public static final int REQUEST_N = 4;
+    public static final int REQUEST_HEIGHT = 5;
+    public static final int REQUEST_TOBJ = 6;
+    public static final int REQUEST_TENV = 7;
 
     private double argR670;
     private double argR690;
@@ -95,6 +95,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private BluetoothDevice selectedDevice;
     private boolean isConnected = false; //用于标记是否连接成功
     private boolean isChoose = false; //用于标记是否在搜索完成前进行了选择
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -104,9 +105,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         etDate = (EditText) findViewById(R.id.et_date);
         etNdvi = (EditText) findViewById(R.id.et_ndvi);
         etLai = (EditText) findViewById(R.id.et_lai);
-        etBiomass  = (EditText) findViewById(R.id.et_biomass);
+        etBiomass = (EditText) findViewById(R.id.et_biomass);
         etChl = (EditText) findViewById(R.id.et_chl);
-        etN  = (EditText) findViewById(R.id.et_n);
+        etN = (EditText) findViewById(R.id.et_n);
         etHeight = (EditText) findViewById(R.id.et_height);
         etTobj = (EditText) findViewById(R.id.et_tobj);
         etTenv = (EditText) findViewById(R.id.et_tenv);
@@ -122,7 +123,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         findViewById(R.id.button_exit).setOnClickListener(this);
 
         deviceNames = new ArrayList<String>();
-        arrayAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,deviceNames);
+        arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, deviceNames);
         btList.setAdapter(arrayAdapter);
 
         /*添加连接蓝牙的监听器*/
@@ -165,25 +166,25 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     /*通过公式计算各个参数，返回结果*/
     private double compute(int requestCode) {
-        switch(requestCode) {
+        switch (requestCode) {
             case REQUEST_NDVI:
-                return ((argR808 - argR670)/(argR808 + argR670));
+                return ((argR808 - argR670) / (argR808 + argR670));
             case REQUEST_LAI:
-                return 0.202*Math.exp(4.535*((argR808 - argR670)/Math.sqrt(argR808 - argR670)));
+                return 0.202 * Math.exp(4.535 * ((argR808 - argR670) / Math.sqrt(argR808 - argR670)));
             case REQUEST_BIO:
-                return 54.81*resultLAI + 42.39;
+                return 54.81 * resultLAI + 42.39;
             case REQUEST_CHL:
-                return -668.319 + 12.652*argR808/argR670 - 5675.885*(argR735/argR690 -1) +
-                        2316.909*(argR735-argR690)/(argR690-argR670);
+                return -668.319 + 12.652 * argR808 / argR670 - 5675.885 * (argR735 / argR690 - 1) +
+                        2316.909 * (argR735 - argR690) / (argR690 - argR670);
             case REQUEST_N:
-                return 242.2 * Math.pow(resultCHL,0.457);
+                return 242.2 * Math.pow(resultCHL, 0.457);
             case REQUEST_HEIGHT:
-                double x = argHeight*2.5/4096;
-                return -36.64*Math.pow(x,3) + 207.71*Math.pow(x, 2) - 376.1*x + 272.56;
+                double x = argHeight * 2.5 / 4096;
+                return -36.64 * Math.pow(x, 3) + 207.71 * Math.pow(x, 2) - 376.1 * x + 272.56;
             case REQUEST_TOBJ:
-                return argTobj/1000;
+                return argTobj / 1000;
             case REQUEST_TENV:
-                return argTonv/1000;
+                return argTonv / 1000;
             default:
                 break;
         }
@@ -223,7 +224,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         dateText = format.format(date);
         etDate.setText(dateText);
         resultNdvi = compute(REQUEST_NDVI);
-        resultLAI  = compute(REQUEST_LAI);
+        resultLAI = compute(REQUEST_LAI);
         resultBio = compute(REQUEST_BIO);
         resultCHL = compute(REQUEST_CHL);
         resultN = compute(REQUEST_N);
@@ -233,14 +234,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         /*计算后得到的结果显示到界面上*/
         etDate.setText(dateText);
-        etNdvi.setText(resultNdvi+"");
-        etLai.setText(resultLAI+"");
-        etBiomass.setText(resultBio+"");
-        etChl.setText(resultCHL+"");
-        etN.setText(resultN+"");
-        etHeight.setText(resultHeight+"");
-        etTobj.setText(resultTobj+"");
-        etTenv.setText(resultTenv+"");
+        etNdvi.setText(resultNdvi + "");
+        etLai.setText(resultLAI + "");
+        etBiomass.setText(resultBio + "");
+        etChl.setText(resultCHL + "");
+        etN.setText(resultN + "");
+        etHeight.setText(resultHeight + "");
+        etTobj.setText(resultTobj + "");
+        etTenv.setText(resultTenv + "");
     }
 
     public void save() {
@@ -248,27 +249,27 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             Toast.makeText(MainActivity.this, "无数据", Toast.LENGTH_SHORT).show();
             return;
         }
-        File file = new File(Environment.getExternalStorageDirectory(),"data_collection.txt");
+        File file = new File(Environment.getExternalStorageDirectory(), "data_collection.txt");
         try {
             if (!file.exists()) {
                 file.createNewFile();
             }
             FileWriter fw = new FileWriter(file, true);
-            BufferedWriter bw  = new BufferedWriter(fw);
-            bw.write("-----------------------------"+"\n");
-            bw.write("日期:" + dateText+ "\n");
-            bw.write("Ndvi:" + resultNdvi+"\n");
+            BufferedWriter bw = new BufferedWriter(fw);
+            bw.write("-----------------------------" + "\n");
+            bw.write("日期:" + dateText + "\n");
+            bw.write("Ndvi:" + resultNdvi + "\n");
             bw.write("LAI:" + resultLAI + "\n");
             bw.write("Biomass:" + resultBio + "\n");
-            bw.write("CHL:" + resultCHL+ "\n");
-            bw.write("N含量:" + resultN+"\n");
+            bw.write("CHL:" + resultCHL + "\n");
+            bw.write("N含量:" + resultN + "\n");
             bw.write("冠层高度:" + resultHeight + "\n");
-            bw.write("目标温度:"  + resultTobj + "\n");
+            bw.write("目标温度:" + resultTobj + "\n");
             bw.write("环境温度:" + resultTenv + "\n");
             bw.flush();
             Toast.makeText(MainActivity.this, "Save successfully", Toast.LENGTH_SHORT).show();
             bw.close();
-        }catch (IOException e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
@@ -293,14 +294,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         if (bluetoothAdapter.isDiscovering()) {
             bluetoothAdapter.cancelDiscovery();
         }
-        if (connectLayout.getVisibility() == View.VISIBLE){
+        if (connectLayout.getVisibility() == View.VISIBLE) {
             connectLayout.setVisibility(View.GONE);
             dataLayout.setVisibility(View.VISIBLE);
             setTitle("待连接");
         }
 
         String name = deviceNames.get(position);
-        for(BluetoothDevice device: devices) {
+        for (BluetoothDevice device : devices) {
             if (device.getName().equals(name)) {
                 selectedDevice = device;
                 Toast.makeText(MainActivity.this, "选择了设备" + device.getName(), Toast.LENGTH_SHORT).show();
@@ -314,41 +315,41 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             Toast.makeText(MainActivity.this, "请选择一个设备", Toast.LENGTH_SHORT).show();
             return;
         }
-       new Thread(new Runnable() {
-           @Override
-           public void run() {
-               try {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
 
-                  // socket = device.createRfcommSocketToServiceRecord(MY_UUID);
+                    socket = device.createRfcommSocketToServiceRecord(MY_UUID);
                    /*通过反射得到BluetoothSocket*/
-                   Method method = device.getClass().getMethod("createRfcommSocket",new Class[]{int.class});
-                   socket = (BluetoothSocket) method.invoke(device,1);
-                   socket.connect();
-                   isConnected = true;
-               } catch (IOException e) {
-                   e.printStackTrace();
-                   try {
-                       socket.close();
-                   } catch (IOException e1) {
-                       e1.printStackTrace();
-                   }
-               } catch (NoSuchMethodException e) {
+                    // Method method = device.getClass().getMethod("createRfcommSocket",new Class[]{int.class});
+                    //socket = (BluetoothSocket) method.invoke(device,1);
+                    socket.connect();
+                    isConnected = true;
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    try {
+                        socket.close();
+                    } catch (IOException e1) {
+                        e1.printStackTrace();
+                    }
+                }/* catch (NoSuchMethodException e) {
                    e.printStackTrace();
                } catch (InvocationTargetException e) {
                    e.printStackTrace();
                } catch (IllegalAccessException e) {
                    e.printStackTrace();
-               }
-               if (isConnected) {
-                  runOnUiThread(new Runnable() {
-                      @Override
-                      public void run() {
-                          setTitle("已连接上"+ device.getName());
-                      }
-                  });
-               }
-           }
-       }).start();
+               }*/
+                if (isConnected) {
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            setTitle("已连接上" + device.getName());
+                        }
+                    });
+                }
+            }
+        }).start();
 
     }
 
@@ -362,7 +363,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             is = socket.getInputStream();
             BufferedReader br = new BufferedReader(new InputStreamReader(is));
             String line;
-            while((line=br.readLine())!=null) {
+            while ((line = br.readLine()) != null) {
                 response += line;
             }
             //把字符数组表示的十六进制转换成double格式
@@ -376,7 +377,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         } catch (IOException e) {
             e.printStackTrace();
-        }finally {
+        } finally {
             try {
                 os.close();
                 is.close();
@@ -389,7 +390,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     /*解析十六进制字符串返回double数值*/
     private double parseHex(String data, int i) {
-        return Integer.parseInt(data.substring(i,i+4), 16);
+        return Integer.parseInt(data.substring(i, i + 4), 16);
     }
 
     private class BluetoothReceiver extends BroadcastReceiver {
@@ -399,8 +400,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             String action = intent.getAction();
             if (BluetoothDevice.ACTION_FOUND.equals(action)) {
                 BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
-                if (device.getBondState()!= BluetoothDevice.BOND_BONDED &&
-                            (deviceNames.indexOf(device.getName())==-1)) {   //设备没有匹配过,且之前没有被添加过，加入列表
+                if (device.getBondState() != BluetoothDevice.BOND_BONDED &&
+                        (deviceNames.indexOf(device.getName()) == -1)) {   //设备没有匹配过,且之前没有被添加过，加入列表
                     try {
                         Method createBondDevice = BluetoothDevice.class.getMethod("createBond"); //进行配对
                         createBondDevice.invoke(device);
@@ -415,9 +416,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     deviceNames.add(device.getName());
                     arrayAdapter.notifyDataSetChanged();             //发生变化时，ListView显示搜索到的蓝牙设备
                 }
-            }else if (BluetoothAdapter.ACTION_DISCOVERY_FINISHED.equals(action)) {
+            } else if (BluetoothAdapter.ACTION_DISCOVERY_FINISHED.equals(action)) {
                 progressBar.setVisibility(View.INVISIBLE);
-                if (!isChoose){
+                if (!isChoose) {
                     Toast.makeText(MainActivity.this, "搜索已经完成", Toast.LENGTH_SHORT).show();
                 }
 
